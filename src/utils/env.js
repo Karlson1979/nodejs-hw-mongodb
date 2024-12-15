2b5b0df727d2b084c9c47c404797276f2f07e568
Tochka <tochka114@gmail.com>
2024-12-15 14:48:32 +0200

    dotenv

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
 export const getEnvVar = (key) => {
   const value = process.env[key];
   if (!value) {
@@ -5,9 +7,8 @@ export const getEnvVar = (key) => {
   }
   return value;
 };
-PORT;
-
-MONGODB_USER;
-MONGODB_PASSWORD;
-MONGODB_URL;
-MONGODB_DB;
+export const PORT = getEnvVar('PORT');
+export const MONGODB_USER = getEnvVar('MONGODB_USER');
+export const MONGODB_PASSWORD = getEnvVar('MONGODB_PASSWORD');
+export const MONGODB_URL = getEnvVar('MONGODB_URL');
+export const MONGODB_DB = getEnvVar('MONGODB_DB');
