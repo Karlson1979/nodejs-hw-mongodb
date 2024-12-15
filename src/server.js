9e27c4eec8517a31a0addaf4f51ba75d97b11372
Tochka <tochka114@gmail.com>
2024-12-15 16:08:39 +0200

    controller

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 import express from 'express'; //
 import cors from 'cors';
 import pino from 'pino-http'; //
-import dotenv from 'dotenv';
-import * as contactServices from './services/contacts.js';
-
-dotenv.config();
+// import dotenv from 'dotenv';
+import contactsRouter from './routers/contacts.js';
+// dotenv.config();
 export const setupServer = () => {
   const app = express();
 
@@ -16,31 +15,7 @@ export const setupServer = () => {
       },
     }),
   );
-  app.get('/contacts', async (req, res) => {
-    const data = await contactServices.getContacts();
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data,
-    });
-  });
-
-  app.get('/contacts/:id', async (req, res) => {
-    const { id } = req.params;
-    const data = await contactServices.getContactById(id);
-    if (!data) {
-      return res.status(404).json({
-        status: 404,
-        message: `Contact with id=${id} not found`,
-      });
-    }
-    res.json({
-      status: 200,
-      message: `Successfully found contact with id ${id}!`,
-      data,
-    });
-  });
-
+  app.use('/contacts', contactsRouter);
   app.use((req, res) => {
     res.status(404).json({
       message: `${req.url} not found`,
