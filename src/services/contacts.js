95734a35f8a82e5aa6edd36c60f14fc7be471b16
Tochka <tochka114@gmail.com>
2025-01-03 12:10:20 +0200

    calcPagination

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,6 +1,18 @@
 import ContactCollection from '../db/models/Contact.js';
+import { calcPaginationData } from '../utils/calcPaginationData.js';
 
-export const getContacts = () => ContactCollection.find();
+export const getContacts = async ({ page = 1, perPage = 10 }) => {
+  const limit = perPage;
+  const skip = (page - 1) * limit;
+  const items = await ContactCollection.find().skip(skip).limit(limit);
+  const total = await ContactCollection.countDocuments();
+  const paginationData = calcPaginationData({ total, page, perPage });
+  return {
+    items,
+    total,
+    ...paginationData,
+  };
+};
 export const getContactById = (id) => ContactCollection.findById(id);
 export const addContact = (payload) => ContactCollection.create(payload);
 export const updateContact = async (_id, payload, options = {}) => {
